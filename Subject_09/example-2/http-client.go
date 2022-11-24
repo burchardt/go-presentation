6ddda709e0704fe266186adc61f68634645d9ef5
burchardt <ryszard.burchardt@nokia.com>
2022-11-24 16:37:00 +0100

    Update http-client.go

--- Subject_09/example-2/http-client.go
+++ Subject_09/example-2/http-client.go
@@ -3,19 +3,31 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
-	response, err := http.Get("http://localhost:8002/hello")
+	const addr = "http://localhost:8002/hello"
+
+	httpClient := http.Client{
+		Timeout: 3 * time.Second,
+	}
+	req, err := http.NewRequest(http.MethodGet, addr, nil)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
+	}
+
+	response, err := httpClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	defer response.Body.Close()
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
 	fmt.Println(string(body))
 }
