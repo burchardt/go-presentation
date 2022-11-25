fad3118d25b23bf140df3011ee32caa5b5956bc7
burchardt <ryszard.burchardt@nokia.com>
2022-11-25 19:06:41 +0100

    Create astronauts_test.go

---
+++ Subject_10/example-3/astronauts_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+type NoProxyWebRequest struct {
+}
+
+func (p NoProxyWebRequest) FetchBody(addr string) ([]byte, error) {
+	return FetchBody(&http.Client{}, addr)
+}
+
+func TestGetHttpClient(t *testing.T) {
+	httpClient, err := getHttpClient(proxy)
+	if err != nil {
+		t.Errorf("Error message, got: %s, want: %s.", err, "nil")
+	}
+
+	if httpClient.Timeout != 3*time.Second {
+		t.Errorf("Timeout, got: %s, want: %s", httpClient.Timeout, 3*time.Second)
+	}
+}
+
+func TestGetAstronauts(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": "OK", "number": 2, "people": [{"name": "Neil Armstrong", "crew": "NASA"}, {"name": "Pete Conrad", "crew": "NASA"}]}`))
+	}))
+	defer server.Close()
+
+	astros, err := getAstronauts(NoProxyWebRequest{}, server.URL)
+	if err != nil {
+		t.Errorf("Error message, got: %s, want: %s.", err, "nil")
+	}
+
+	if astros.Number != 2 {
+		t.Errorf("Number of people in space, got: %d, want: %d.", astros.Number, 2)
+	}
+
+	if astros.Message != "OK" {
+		t.Errorf("Message, got: %s, want: %s.", astros.Message, "OK")
+	}
+
+	if len(astros.Crew) != 2 {
+		t.Errorf("Number of Crew members, got: %d, want: %d.", len(astros.Crew), 2)
+	}
+
+}
